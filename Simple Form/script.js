87fc621219743bd7f0feeb5d8bb7284896157be4
Prodip Ray <65656694+prodipalways@users.noreply.github.com>
2026-02-23 21:53:12 +0600

    Update script.js

--- Simple Form/script.js
+++ Simple Form/script.js
@@ -1,13 +1,13 @@
-document.getElementById("registrationForm").addEventListener("submit", function (e) {
+document.getElementById("reginstrationForm").addEventListener("submit", function (e) {
     e.preventDefault();
 
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
-    let passoerd = document.getElementById("password").value;
+    let password = document.getElementById("password").value;
 
     if (name === "" || email === "" || password === "") {
         alert("Please fill all fields");
     }
 
-    alert("Registration Successful!");
+    alert("Registration Successful")
 })
